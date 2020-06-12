3ada14f58590b1c1a66223b9ba11fc23bfa6f60f
Heathcliff-Hu <hrc530756164@gmail.com>
2020-06-12 09:27:08 +0800

    feat: 修改鉴权方式

--- src/middleware/security.js
+++ src/middleware/security.js
@@ -1,27 +1,18 @@
-const _ = require('lodash')
-
 const {forEachOperation} = require('../utility')
-
-const BINDING_KEY = '__security_middleware__'
-const HANDLER_NAME = '__name__'
 
 module.exports = (api, middlewares) => {
   const definition = api.definition
   // pre-bind operation to security middleware
   forEachOperation(definition, (path, method, operationDef) => {
-    let securities = operationDef.security || []
+    let securitys = operationDef.security || []
     const globalSecurity = definition.security || []
-    securities = securities.concat(globalSecurity)
+    securitys = securitys.concat(globalSecurity)
 
-    const securityDefinitions = definition.securityDefinitions
-    let securityHandlers = securities.map((security) => {
+    operationDef['securities'] = []
+    securitys.map((security) => {
       security = Object.keys(security)[0]
-      const handler = middlewares[securityDefinitions[security]['x-securityHandler']]
-      handler[HANDLER_NAME] = security
-      return handler
+      operationDef['securities'].push(security)
     })
-    securityHandlers = _.uniq(securityHandlers)
-    operationDef[BINDING_KEY] = securityHandlers
   })
 
   return (req, res, done) => {
@@ -29,23 +20,7 @@ module.exports = (api, middlewares) => {
       return done()
     }
 
-    let index = 0
-    const securityHandlers = req.swagger.operation.definition[BINDING_KEY]
-    const next = (err) => {
-      // fast return on first successful authentication
-      if (index !== 0 && !err) {
-        req.security = securityHandlers[index - 1][HANDLER_NAME]
-        return done()
-      }
-      // return last authentication failure
-      if (index === securityHandlers.length) {
-        return done(err)
-      }
-      // TODO: support additional arguments for security middleware
-      let securityHandler = securityHandlers[index]()
-      index = index + 1
-      securityHandler(req, res, next)
-    }
-    next()
+    req.securities = req.swagger.operation.definition.securities;
+    return middlewares['security']()(req, res, done);
   }
 }
